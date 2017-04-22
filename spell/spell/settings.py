b30aaca86bee7feb68113f3ba7feb7dd87e05e4b
Po <pranav292gpt@gmail.com>
2017-04-23 05:06:55 +0530

    Changed db

--- spell/spell/settings.py
+++ spell/spell/settings.py
@@ -78,12 +78,23 @@ WSGI_APPLICATION = 'spell.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+            'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'spell',
+            'USER': 'api_admin',
+            'PASSWORD': 'password',
+            'HOST': 'localhost',
+            'PORT': '',
+             }
+    }
+'''
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
