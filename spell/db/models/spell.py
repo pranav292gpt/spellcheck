b923dfee76086b52e795786b35dda3fa145f129c
Po <pranav292gpt@gmail.com>
2017-04-23 05:23:55 +0530

    Added index and Gitignore file

--- spell/db/models/spell.py
+++ spell/db/models/spell.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class WordFrequency(models.Model):
-    word = models.CharField(max_length=64)
+    word = models.CharField(max_length=64, db_index=True)
     frequency = models.IntegerField()
